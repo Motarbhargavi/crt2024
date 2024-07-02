e8c6f12734668daca5ad4c5c9a90478af7e96fef
Motarbhargavi <bhargavi26102003@gmail.com>
2024-07-02 13:36:28 +0530

    Create Lowercase.java

---
+++ Lowercase.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Lowercase
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String str="java IS simple languAGE";
+System.out.println(str.toLowerCase());
+System.out.println(str.toUpperCase());
+}
+}
